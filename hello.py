6c013287306d7cba7d615e76f9c3f0c73e1c7885
Bartequa Blackmon <92996483+BartequaBlackmon@users.noreply.github.com>
2023-09-29 16:07:51 +0000

    adding a simple debut

--- hello.py
+++ hello.py
@@ -2,3 +2,17 @@ def addthis(x,y):
     return x+y
 print(addthis(1,2))
 
+def addthis(x,y):
+    import pdb;pdb.set_trace()
+    print(f"This is x: {x} and the x-type {type(x)}")
+    print(f"This is y: {y} and the y-type is {type(y)}")
+
+    try:
+        result = x+y
+    except TypeError:
+        print(f"The wrong type passed")
+        result = int(x)+int(y)
+
+    print(f"This is the result: {result}")
+    return result
+print(addthis("one", 2))
